9e96ae87275524f0e2c5472a2e8e8087d71a0022
Kyla Kolb <kkolb@mitre.org>
2014-06-12 11:42:43 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flagcam::Application.config.secret_key_base = '85352242a20f0bf40c7f80f2002a810be767c3043d836d766a8d6ba9240a65b2c31e2256c27c94ae20b9d3875f87e440a7d3c770ed9de075eeb950a99312d0bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flagcam::Application.config.session_store :cookie_store, key: '_flagcam_session'
