046aff649fe87fe0d9ad48d0d34951114b8eadd8
Kyla Kolb <kkolb@mitre.org>
2014-06-20 14:56:19 -0400

    Double MD5 hash, so competitors can't get the image naming convention.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ require 'digest'
 
 class HomeController < ApplicationController
   def index
-  	@img = Digest::MD5.hexdigest("1x8x0") << ".jpeg"
+  	@img = Digest::MD5.hexdigest(Digest::MD5.hexdigest("1x8x0")) << ".jpeg"
   end
 end
--- lib/tasks/rename.rb
+++ lib/tasks/rename.rb
@@ -3,7 +3,7 @@ path = "" #path to directory
 Dir.entries(path).sort.each do |entry|
 	if entry.chars.first != "."
 		origin = File.basename(entry, File.extname(entry))
-		newEntry = Digest::MD5.hexdigest(origin.gsub("pano_adams_Library_450px [www.imagesplitter.net]-", '').gsub('-','x') << "x0") << ".jpeg" #remane file to MD5 Hash of LATITUDExLONGITUDExZOOM
+		newEntry = Digest::MD5.hexdigest(Digest::MD5.hexdigest(origin.gsub("pano_adams_Library_450px [www.imagesplitter.net]-", '').gsub('-','x') << "x0")) << ".jpeg" #remane file to MD5 Hash of LATITUDExLONGITUDExZOOM
 		File.rename( "#{path}/#{entry}", "#{path}/#{newEntry}" )
 		puts "Rename from " + origin + " to " + newEntry
 	end
