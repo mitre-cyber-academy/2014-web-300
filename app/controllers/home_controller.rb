df924f601817d6ede07a9ad7a2cfe07be371e9e3
Kyla Kolb <kkolb@mitre.org>
2014-06-23 15:22:32 -0400

    Added flash notice for not working buttons.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
   def index
   	@img = Digest::MD5.hexdigest(Digest::MD5.hexdigest("1x8x0")) << ".jpeg"
   end
+
+  def buttons
+  	redirect_to :back,flash:{info:"Sorry, Bro. Still integrating the API for that."}
+  end
 end
